878d79a772687e38cc83107be13c27a22b43d097
Prachi Tripathi <114851153+tripathiprachi18@users.noreply.github.com>
2023-01-25 13:07:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
+function generateCodeFromObject(obj){
+    //return a code generated string
+   }
+   
+module.exports=generateCodeFromObject;
